47d18d3518db0a09670b7ab00f9e9e5ae4dbbf34
QMaximillian <quinnlashinsky@gmail.com>
2018-08-18 13:23:27 -0400

    Built more backend routes for songs_controller

--- app/controllers/api/v1/songs_controller.rb
+++ app/controllers/api/v1/songs_controller.rb
@@ -6,12 +6,37 @@ module Api
         render json: Song.all
       end
 
-      def create
-      @song = User.new(song_params)
+      def show
+        @song = Song.find(params[:id])
+        render json: @song
+      end
 
-        if @song.save
-          render json: @song
-        end
+      def create
+        @song = Song.create(song_params)
+        render json: @song
+      end
+
+      def edit
+
+      end
+
+      def update
+        @song = Song.find(params[:id])
+        @song.name = song_params[:name]
+				@song.lyric = song_params[:lyric]
+				@song.music = song_params[:music]
+				@song.user_id = song_params[:user_id]
+				@song.save
+				render json: @song
+      end
+
+      def destroy
+        @song = Song.find(params[:id])
+
+        User.all.each do |song|
+          if user.id == song.user_id
+            user.destroy
+          end
       end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :songs
       resources :users
-      resources :lyric_musics
     end
   end
 end
